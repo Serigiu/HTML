6f4ae50e55ee41a3809e1eceb29edb0e1a836c33
Sergiu Marandescu <sergiu.marandescu@gmail.com>
2022-12-05 20:54:39 +0200

    05.12.22 - ora a treia si Execitiul3(html part)

--- Index2.js
+++ Index2.js
@@ -1,6 +1,8 @@
 function changeColor (){
     var button = document.querySelector("#bottom-button")
     console.log(button.classList)
-    button.classList.remove("background-lightgreen")
-    button.classList.add("background-lightblue")
+    button.classList.remove("text-bg-success")
+    button.classList.add("text-bg-primary")
+    console.log(button.innerHTML)
+    button.innerHTML = "You are welcome!"
 }
